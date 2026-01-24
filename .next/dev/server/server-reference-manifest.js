913bfeead14d9ecfc4f663e45471954d3b082458
Zekri Akram <109292318+ZekriDev1@users.noreply.github.com>
2026-01-24 13:20:39 +0100

    main commit

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vUNZ9y0Z0vTSuR7uYPvZLNF3eRlNxyZOgQLL2qjIchE=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_fc449437._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_41133afa._.js",
-  "static/chunks/node_modules_4fbe0b8d._.js"
-],
-    source: "dynamic"
-});
